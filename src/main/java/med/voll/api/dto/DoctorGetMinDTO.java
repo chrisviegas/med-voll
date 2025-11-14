5e618a366aa533ab8d3b70ef4735fda02d4e57bd
Christian Viegas <chris.gabriel346@gmail.com>
2025-11-14 17:10:55 -0300

    feat(doctor): modify the DoctorGetMinDTO
    Add the id attribute, so that when the api receives a GET request,
    the client receives the JSON with the id of each doctor.

--- src/main/java/med/voll/api/dto/DoctorGetMinDTO.java
+++ src/main/java/med/voll/api/dto/DoctorGetMinDTO.java
@@ -3,9 +3,9 @@ package med.voll.api.dto;
 import med.voll.api.entities.Doctor;
 import med.voll.api.enums.DoctorSpecialtyEnum;
 
-public record DoctorGetMinDTO(String name, String email, String crm, DoctorSpecialtyEnum specialty){
+public record DoctorGetMinDTO(Long id,String name, String email, String crm, DoctorSpecialtyEnum specialty){
 
     public DoctorGetMinDTO(Doctor doctor) {
-        this(doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getDoctorSpecialtyEnum());
+        this(doctor.getId(),doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getDoctorSpecialtyEnum());
     }
 }
